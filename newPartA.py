5ccc084170bfb298fc139febfdace12959d2880f
trannathy <trannathy@gmail.com>
2026-04-02 13:47:36 -0700

    revised tokenize to account for puncutation

--- newPartA.py
+++ newPartA.py
@@ -1,20 +1,17 @@
-
+from string import puncutation
 class Token:
     def __init__(self, value: str):
-        self._value = value
+        self._value = value.lower()
 
     def value(self):
         return self._value
-
-    
-
     
 class TokenList:
-    def __init__(self):
-        self.tokens = []
-        self.frequencies = {}
 
     def tokenize(self, TextFilePath: str) -> list[Token]:
+
+        #NEED TO CHANGE THIS TO SUPPORT PUNCUTATION
+
         ''' Write a method/function that reads in a text file and returns a 
         list of the tokens in that file. For the purposes of this project,
         a token is a sequence of alphanumeric characters, independent of
@@ -24,12 +21,13 @@ class TokenList:
         write a tokenizer. '''
 
         with open(TextFilePath, "r") as file:
-            allWords = [Token(word.lower()) for word in file.read().split() if word.isascii()]
+            fileAsStr = file.read()
 
-        self.tokens = allWords
+        for character in puncutation:
+            fileAsStr.replace(character, " ")
+            
+        allWords = [Token(word) for word in fileAsStr.split() if word.isascii()]
         return allWords
-        
-        
 
     def computeWordFrequencies(tokenList: list[Token]) -> dict:
         ''' returns Map<Token,Count>  
@@ -46,8 +44,7 @@ class TokenList:
                 mappedFrequencies[token.value] = 1
         return mappedFrequencies
 
-
-    def print() -> None:
+    def print(frequencyMapping: dict) -> None:
         '''Finally, write a method that prints out the word frequency count
         onto the screen. The print out should be ordered by decreasing 
         frequency (so, the highest frequency words first). 
@@ -56,13 +53,19 @@ class TokenList:
 
         <token>\t<freq>
         <token> <freq>
-        <token> - <freq>
+                                 <token> - <freq>
         <token> = <freq>
         <token> > <freq>
         <token> -> <freq>
         <token> => <freq>'''
-        pass
-    
+
+        #sorted method originated from https://flexiple.com/python/python-sort-dictionary-by-value
+        flippedList = sorted((value, key) for (key, value) in frequencyMapping.items())
+        sortedMapping = dict([k, v] for v, k in flippedList.reverse())
+
+        for token, freq in enumerate(sortedMapping):
+            print(f'{token} - {freq}')
+        
     
 if __name__ == "main":
     pass
